338250912b46412a580ddba63be4db19eddc30ba
rustystylus <rustystylus@gmail.com>
2014-12-07 19:58:30 +0000

    change up to down typo

--- app/views/answers/show.blade.php
+++ app/views/answers/show.blade.php
@@ -3,7 +3,7 @@
     <h1>Showing {{ $answer->id }}</h1>
     Vote
     <a href="#">Up {{ $answer->upvotes }}<span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span></a>
-    <a href="#">Up {{ $answer->downvotes }}<span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>
+    <a href="#">Down {{ $answer->downvotes }}<span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>
 </div>
 
 <div class="row">
